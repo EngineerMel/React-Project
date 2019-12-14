b3e0a1b0305cf62b7e882511f7d47730440a4332
EngineerMel <ramelhaines25@gmail.com>
2019-12-13 23:05:32 -0500

    added delete button to table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,7 +5,6 @@ class Movies extends Component {
   state = {
     movies: getMovies()
   };
-
   render() {
     return (
       <table className="table">
@@ -15,6 +14,7 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -24,6 +24,9 @@ class Movies extends Component {
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button className="btn btn-danger btn-sm">Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>
