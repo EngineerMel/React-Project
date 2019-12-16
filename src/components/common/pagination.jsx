eb17ff4af3894ac965fd9bd38082e102a4d5eea9
EngineerMel <ramelhaines25@gmail.com>
2019-12-16 17:13:10 -0500

    installed lodash into the project, and mapped pages to a list item

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import _ from "lodash";
 
 const Pagination = props => {
-  return null;
+  const { itemsCount, pageSize } = props;
+
+  const pagesCount = itemsCount / pageSize;
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(page => (
+          <li className="page-item">
+            <a className="page-link">1</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
