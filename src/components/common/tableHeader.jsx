8d6966be34248729efa42ce6d3696e3eeaddc802
EngineerMel <ramelhaines25@gmail.com>
2019-12-20 14:16:20 -0500

    successfully extracted tableheader

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -15,12 +15,18 @@ class TableHeader extends Component {
     }
     this.props.onSort(sortColumn);
   };
+
   render() {
     return (
       <thead>
         <tr>
-          {this.props.column.map(column => (
-            <th></th>
+          {this.props.columns.map(column => (
+            <th
+              key={column.path || column.key}
+              onClick={() => this.raiseSort(column.path)}
+            >
+              {column.label}
+            </th>
           ))}
         </tr>
       </thead>
